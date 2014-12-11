c03aa828b492e101fd20bface41051422ccbfe37
xiaojinwei <xiaojinwei9@mail.com>
2014-12-11 22:31:36 +0800

    init recb

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,4 +18,30 @@ public class Application extends Controller {
         render();
     }
 
+    public static void topMenu(){
+    	render();
+    }
+    public static void com(){
+    	render();
+    }
+    public static void pro(){
+    	render();
+    }
+    public static void know(){
+    	render();
+    }
+    public static void about(){
+    	render();
+    }
+    
+    public static void saveTestTb(Long userId,String userName){
+    	Logger.info("func[saveTestTb]userId["+userId+"]userName["+userName+"]");
+    	HashMap<String,Object> result =new HashMap<String,Object>();
+    	TestTb tb=new TestTb(userId,userName);
+    	tb.save();
+    	result.put("code", "100");
+    	result.put("msg", "操作成功");
+    	Logger.info("func[saveTestTb]userId["+userId+"]userName["+userName+"]result["+result+"]");
+    	renderJSON(result);
+    }
 }
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,5 +1,8 @@
+import org.codehaus.groovy.tools.shell.util.Logger;
 import org.junit.*;
+
 import java.util.*;
+
 import play.test.*;
 import models.*;
 
@@ -10,4 +13,11 @@ public class BasicTest extends UnitTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void TestTb(){
+    	play.Logger.info("TestTbGo");
+    	TestTb tb=new TestTb(1L,"xiao");
+    	tb.save();
+    	play.Logger.info("TestTbGoEnd");
+    }
 }
