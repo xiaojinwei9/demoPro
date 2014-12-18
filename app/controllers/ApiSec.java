d68904846efa44bcecbab8b13c28adb77bba51c2
xiaojinwei9 <xiaojinwei9@gmail.com>
2014-12-18 18:01:52 +0800

    md5

--- app/controllers/ApiSec.java
+++ app/controllers/ApiSec.java
@@ -1,17 +1,25 @@
 package controllers;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import play.Logger;
 import play.mvc.Before;
 import play.mvc.Controller;
+import utils.StringUtils;
 
 public class ApiSec extends Controller {
 
 	@Before
 	static void checkToken(){
 		Logger.info("checkToken...");
-		 Map<String, String> paramsAll=params.allSimple();
-		 Logger.info("params:"+paramsAll);
+		 Map<String, String> paramsMap=params.allSimple();
+		 Logger.info("params:"+paramsMap);
+		 Object[] paramsKeys=paramsMap.keySet().toArray();
+		  Arrays.sort(paramsKeys);
+		  for (int i = 0; i < paramsKeys.length; i++) {
+			     Logger.info("paramsKeys>"+paramsKeys[i]+">"+paramsMap.get(paramsKeys[i]));
+			}
+		 Logger.info("md5"+StringUtils.md5("aaabbbccc"));
 	}
 }
